5b7d7c501f5113f49d89b7ae3d34a8ff860cc470
4prathamesh <wprathamesh2@gmail.com>
2023-10-22 07:53:15 +0530

    patterns question

--- patterns_5_different_type_of_solid_half_dimond.c
+++ patterns_5_different_type_of_solid_half_dimond.c
@@ -5,6 +5,7 @@
 // 555
 // 44
 // 3
+/*
 #include<stdio.h>
 int main()
 {
@@ -35,6 +36,32 @@ int main()
             --a;
         }
         
+        printf("\n");
+    }
+    return 0;
+}
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=8;i++)
+    {
+        for(j=1;j<=7;j++)
+        {
+            if(j%2!=0 && j<=i)
+            {
+                printf("%d",i);
+            }
+            else if(j%2==0 && j<=i)
+            {
+                printf("*");
+                
+            }
+            else{
+                printf(" ");
+            }
+        }
         printf("\n");
     }
     return 0;
